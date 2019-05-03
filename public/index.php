154a4e2d18d36eae12c1ec118b1c1e6dc651203d
JUN <soipon0@gmail.com>
2019-05-03 09:16:43 +0900

    index.php の用意

--- public/index.php
+++ public/index.php
@@ -1,3 +1,27 @@
 <?php
 
 echo "Hello MyWorld";
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Image Uploader</title>
+    <style>
+    body {
+        text-align: center;
+        font-family: Arial, sans-serif;
+    }
+    </style>
+</head>
+<body>
+    
+    <form action="" method="post" enctype="multipart/form-data">
+    <input type="hidden" name="MAX_FILE_SIZE">
+    <input type="file" name="image">
+    <input type="submit" value="upload">
+    </form>
+</body>
+</html>
