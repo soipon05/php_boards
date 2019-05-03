1467a16e5bdbad17b03ab196721e9263aeda73ce
JUN <soipon0@gmail.com>
2019-05-03 10:08:18 +0900

    投稿ファイルのチェック

--- public/ImageUploader.php
+++ public/ImageUploader.php
@@ -22,6 +22,21 @@ class ImageUploader {
     }
 
     private function _validateUpload() {
-        
+        // var_dump($_FILES);
+        // exit;
+
+        if (!isset($_FILES['images']) || !isset($_FILES['image']['errors'])) {
+            throw new \Exception('Upload Error!');
+        }
+
+        switch ($_FILES['image']['error']) {
+            case 'UPLOAD_ERR_OK':
+                return true;
+            case UPLOAD_ERR_INT_SIZE:
+            case UPLOAD_ERR_FORM_SIZE:
+                throw new \Exception('File too large!');
+            default:
+                throw new \Exception("Err: " . $_FILES['image']['error']);
+        }
     }
 }
